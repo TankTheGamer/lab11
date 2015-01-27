87a6612053b84ee9ea8c5c285d8baf5647161eac
tankthegamer <tankdabank245@gmail.com>
2015-01-27 11:11:33 -0600

    First commit

---
+++ Lab112darrays/src/Runner.java
@@ -0,0 +1,50 @@
+
+public class Runner {
+	public static void main(String[] args) {
+		int[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][] wtfisthis
+		= new int[1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1][1];
+	}	
+	
+	public static int max(int[][] nums){
+		int toReturn;
+		return 0;
+		
+	}
+	
+	public static int rowSum(int[][] nums, int x){
+		return x;
+		
+	}
+	public static int columnSum(int[][] nums, int x){
+		return x;
+		
+	}
+	public static int[] allRowSums(int[][] nums){
+		return null;
+		
+	}
+	public static boolean isRowMagic(int[][] nums){
+		return false;
+		
+	}
+	public static boolean isColumnMagic(int[][] nums){
+		return false;
+		
+	}
+	public static boolean isSquare(int[][] nums){
+		return false;
+		
+	}
+	public static boolean isMagic(int[][] nums){
+		return false;
+		
+	}
+	public static boolean isLatin(int[][] nums){
+		return false;
+		
+	}
+	public static boolean inSequence(int[][] nums){
+		return false;
+		
+	}
+}
